471f816f51e5bff20a2b275876df96b48e9db407
Yuuki883 <yuukisaitou14@gmail.com>
2020-01-25 12:51:32 +0000

    admin&category modified

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,14 +1,25 @@
 class Admin::CategoriesController < Admin::AdminapplicationsController
   def index
+    @category = Category.new
+    @categories = Category.all
   end
 
   def edit
   end
 
   def create
+    @category = Category.new(category_params)
+    @category.save
+    # redirect_to admin_categories_path
   end
 
   def update
+    @category = category.find(params[:id])
+    @category.update(category_params)
   end
 
+ private
+  def category_params
+     params.require(:category).permit(:sweets_name)
+  end
 end
