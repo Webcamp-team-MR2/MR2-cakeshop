09f18955ae0fc1d2794499014e9122a5c65e1d64
saeko9741 <saeco9741@gmail.com>
2020-01-26 12:45:30 +0000

    created cart_items page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create!(
-  [
-    {
-      sweets_name: 'cake',
-    }
-  ]
-)
-Item.create!(
-  [
-    {
-      image_id: '',
-      price: '1100',
-      name: 'いちごのショートケーキ',
-      category_id: '1',
-    },
-    {
-      image_id: '',
-      price: '330',
-      name: 'チーズタルト',
-      category_id: '1',
-    }
-  ]
-)
+# Category.create!(
+#   [
+#     {
+#       sweets_name: 'cake',
+#     }
+#   ]
+# )
+# Item.create!(
+#   [
+#     {
+#       image_id: '',
+#       price: '1100',
+#       name: 'いちごのショートケーキ',
+#       category_id: '1',
+#     },
+#     {
+#       image_id: '',
+#       price: '330',
+#       name: 'チーズタルト',
+#       category_id: '1',
+#     }
+#   ]
+# )
 
 CartItem.create!(
   [
+    # {
+    #   item_id: '1',
+    #   customer_id: '1',
+    #   count: '1',
+    # },
     {
-      item_id: '1',
+      item_id: '2',
       customer_id: '1',
-      count: '1',
+      count: '2',
     }
   ]
 )
