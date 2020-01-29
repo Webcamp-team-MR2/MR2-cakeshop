901e1f1547670c746c184d33b753696201a5523c
shun-droid0227 <thermohygro1991@gmail.com>
2020-01-29 10:31:38 +0000

    add time

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,8 @@
 class Admin::CustomersController < Admin::AdminapplicationsController
   def top
+    @order_counts = Order.where("created_at between '#{Date.today} 0:00:00' and '#{Date.today} 23:59:59'")
+    # @order_counts = Order.where(created_at: 1.day.ago.all_day)
+    
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,38 +14,23 @@
 #     }
 #   ]
 
-Item.create!(
-  [
-    {
-      image_id: '',
-      price: '1100',
-      name: 'いちごのショートケーキ',
-      category_id: '56',
-    },
-    {
-      image_id: '',
-      price: '330',
-      name: 'チーズタルト',
-      category_id: '55',
-    }
-  ]
-)
-Customer.create!(
-[
- {
-  email: 'mr2@mr2',
-  password: 'mr2pass',
-  first_name: 'shun',
-  last_name: 'yamashiro',
-  last_name_kana: 'シュン',
-  first_name_kana:'ヤマシロ',
-  postal_code:'1980024',
-  address:'japan,tokyo',
-  tel: '000000000',
-  customer_status: 'true',
- }
-]
-)
+# Item.create!(
+#   [
+#     {
+#       image_id: '',
+#       price: '1100',
+#       name: 'いちごのショートケーキ',
+#       category_id: '56',
+#     },
+#     {
+#       image_id: '',
+#       price: '330',
+#       name: 'チーズタルト',
+#       category_id: '55',
+#     }
+#   ]
+# )
+# )
 Order.create!(
  [
   {
@@ -62,21 +47,21 @@ Order.create!(
  OrderItem.create!(
   [
    {
-    order_id: '1',
+    order_id: '2',
     item_id:'1',
     price:'5000',
     count: '2',
     create_status: '0',
    },
    {
-    order_id: '1',
+    order_id: '2',
     item_id:'1',
     price:'2000',
     count: '4',
     create_status: '0',
    },
    {
-    order_id: '1',
+    order_id: '2',
     item_id:'1',
     price:'15000',
     count: '20',
