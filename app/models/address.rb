830ea28d80a6b713843fd902be0f49a733d70b65
saki918 <hiro9181013@gmail.com>
2020-02-03 11:01:24 +0900

    バリデーション 郵便番号の入力を数字限定に変更

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
 	belongs_to :customer
 
-	validates :postal_code, presence: true
+	validates :postal_code, format: { with: /\A[0-9０-９]+\z/}
   validates :address, presence: true
   validates :full_name, presence: true
 end
