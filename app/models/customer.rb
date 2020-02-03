7591442c51ba2f1eb028711546a7c86f409d9ee1
saki918 <hiro9181013@gmail.com>
2020-02-03 13:11:04 +0900

    モデルのバリデーション 詳細指定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,13 +7,13 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :postal_code, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :postal_code, format: { with: /\A[0-9０-９]+\z/}
   validates :address, presence: true
-  validates :tel, presence: true
+  validates :tel, format: { with: /\A[0-9０-９]+\z/}
   # validates :customer_status, inclusion: {in: [true, false]}
   def active_for_authentication?
     super && customer_status?
